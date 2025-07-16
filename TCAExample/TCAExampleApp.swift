2078a3b214b27b760c79114f2ef2376447dfe0d5
Harpreet Singh <singhsharpreet3@gmail.com>
2025-07-16 21:37:01 +0530

    Initial Commit

---
+++ TCAExample/TCAExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAExampleApp.swift
+//  TCAExample
+//
+//  Created by Harpreet Singh on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TCAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
